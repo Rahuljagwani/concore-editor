ccb36861d73d703dca5ceb91267ac43b7aa6dd38
Rahul <rahuljagwani1012@gmail.com>
2023-07-06 00:41:59 +0530

    stop workable in loader

--- src/component/modals/Loader.jsx
+++ src/component/modals/Loader.jsx
@@ -3,23 +3,28 @@ import BeatLoader from 'react-spinners/BeatLoader';
 import ParentModal from './ParentModal';
 import './Loader.css';
 
-const Loader = ({ superState }) => (
-    <ParentModal ModelOpen={superState.loader} title="Wait.... The function is being executed">
-        <div className="loader-main">
-            <BeatLoader color="#36d7b7" className="loader-items" />
-            <BeatLoader color="#36d7b7" className="loader-items" />
-            <BeatLoader color="#36d7b7" className="loader-items" />
-            <BeatLoader color="#36d7b7" className="loader-items" />
-            <BeatLoader color="#36d7b7" className="loader-items" />
-            <BeatLoader color="#36d7b7" className="loader-items" />
-            <BeatLoader color="#36d7b7" className="loader-items" />
-        </div>
-        <br />
-        <br />
-        { superState.dockerCheck && superState.graphs[superState.curGraphIndex].built
-            ? <button className="btn loader-btn" type="button">STOP Docker Build</button>
-            : '' }
-    </ParentModal>
-);
+const Loader = ({ superState }) => {
+    const stopDocker = () => {
+        superState.curGraphInstance.stop();
+    };
+    return (
+        <ParentModal ModelOpen={superState.loader} title="Wait.... The function is being executed">
+            <div className="loader-main">
+                <BeatLoader color="#36d7b7" className="loader-items" />
+                <BeatLoader color="#36d7b7" className="loader-items" />
+                <BeatLoader color="#36d7b7" className="loader-items" />
+                <BeatLoader color="#36d7b7" className="loader-items" />
+                <BeatLoader color="#36d7b7" className="loader-items" />
+                <BeatLoader color="#36d7b7" className="loader-items" />
+                <BeatLoader color="#36d7b7" className="loader-items" />
+            </div>
+            <br />
+            <br />
+            { superState.dockerCheck && superState.graphs[superState.curGraphIndex].built
+                ? <button className="btn loader-btn" onClick={stopDocker} type="button">STOP Docker Build</button>
+                : '' }
+        </ParentModal>
+    );
+};
 
 export default Loader;
