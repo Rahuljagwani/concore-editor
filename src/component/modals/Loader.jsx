6c3cb3ab7528b634d53acc51ef0d6c579ca554b2
Rahul <rahuljagwani1012@gmail.com>
2023-07-06 00:29:17 +0530

    stop button added in loader

--- src/component/modals/Loader.jsx
+++ src/component/modals/Loader.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import BeatLoader from 'react-spinners/BeatLoader';
 import ParentModal from './ParentModal';
+import './Loader.css';
 
 const Loader = ({ superState }) => (
-    <ParentModal ModelOpen={superState.loader} title="Wait........">
-        <BeatLoader color="#36d7b7" />
+    <ParentModal ModelOpen={superState.loader} title="Wait.... The function is being executed">
+        <div className="loader-main">
+            <BeatLoader color="#36d7b7" className="loader-items" />
+            <BeatLoader color="#36d7b7" className="loader-items" />
+            <BeatLoader color="#36d7b7" className="loader-items" />
+            <BeatLoader color="#36d7b7" className="loader-items" />
+            <BeatLoader color="#36d7b7" className="loader-items" />
+            <BeatLoader color="#36d7b7" className="loader-items" />
+            <BeatLoader color="#36d7b7" className="loader-items" />
+        </div>
+        <br />
+        <br />
+        { superState.dockerCheck && superState.graphs[superState.curGraphIndex].built
+            ? <button className="btn loader-btn" type="button">STOP Docker Build</button>
+            : '' }
     </ParentModal>
 );
 
